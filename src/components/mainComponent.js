12ac1cf006f114634705933ecf743b79963a4ca4
golicin.a <b.golicin@gmail.com>
2020-07-09 15:14:12 +0300

    added about,contact components, react-router, breadcrumbs, navigavion

--- src/components/mainComponent.js
+++ src/components/mainComponent.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import Header from "./headerComponent";
 import Footer from "./footerComponent";
-import Menu from "./menuComponent";
+import Contact from "./contactComponent";
 import DishDetail from "./dishDetailComponent";
+import About from "./aboutComponent";
+import Menu from "./menuComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./homeComponent";
 
@@ -12,13 +17,47 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS,
         };
     }
 
-
     render() {
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail
+                    dish={
+                        this.state.dishes.filter(
+                            (dish) =>
+                                dish.id === parseInt(match.params.dishId, 10),
+                        )[0]
+                    }
+                    comments={this.state.comments.filter(
+                        (comment) =>
+                            comment.dishId ===
+                            parseInt(match.params.dishId, 10),
+                    )}
+                />
+            );
+        };
+
         const HomePage = () => {
-            return <Home />;
+            return (
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={
+                        this.state.promotions.filter(
+                            (promo) => promo.featured,
+                        )[0]
+                    }
+                    leader={
+                        this.state.leaders.filter(
+                            (leader) => leader.featured,
+                        )[0]
+                    }
+                />
+            );
         };
 
         return (
@@ -30,6 +69,13 @@ class Main extends Component {
                         exact
                         path="/menu"
                         component={() => <Menu dishes={this.state.dishes} />}
+                    />
+                    <Route path="/menu/:dishId" component={DishWithId} />
+                    <Route exact path="/contactus" component={Contact} />
+                    <Route
+                        exact
+                        path="/aboutus"
+                        component={() => <About leaders={this.state.leaders} />}
                     />
                     <Redirect to="/home" />
                 </Switch>
